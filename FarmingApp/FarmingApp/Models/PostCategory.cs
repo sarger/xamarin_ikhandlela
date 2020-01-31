a1f78605f2c3be235015e776bf94e5560077ed95
sarger <siyabonga2010@gmail.com>
2020-01-31 09:38:07 +0200

    commit

---
+++ FarmingApp/FarmingApp/Models/PostCategory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarmingApp.Models
+{
+
+    public class PostCategory
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Category
+        {
+            get;
+            set;
+        }
+
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ FarmingApp/FarmingApp/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FarmingApp.Models
+{
+   public class User
+    {
+    }
+}
