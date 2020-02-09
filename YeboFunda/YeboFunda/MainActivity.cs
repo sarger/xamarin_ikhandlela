9c15ac3cdae279b53f89ed2db6a1f5a4840ceb4b
mnemthuli <mnelisi.mthuli@spar.co.za>
2020-02-09 10:18:50 +0200

    commit

---
+++ YeboFunda/YeboFunda/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+
+namespace YeboFunda
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, BottomNavigationView.IOnNavigationItemSelectedListener
+    {
+        WebView textMessage;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            textMessage = FindViewById<WebView>(Resource.Id.message);
+            BottomNavigationView navigation = FindViewById<BottomNavigationView>(Resource.Id.navigation);
+            navigation.SetOnNavigationItemSelectedListener(this);
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+        public bool OnNavigationItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.navigation_home:
+                    textMessage.LoadUrl("https://www.google.com");
+
+                    return true;
+                case Resource.Id.navigation_dashboard:
+                    textMessage.LoadUrl("https://www.mnelisi.com");
+
+                    return true;
+                case Resource.Id.navigation_notifications:
+                    textMessage.LoadUrl("https://www.mnelisi.com/Mobile");
+
+                    return true;
+            }
+            return false;
+        }
+    }
+}
+
